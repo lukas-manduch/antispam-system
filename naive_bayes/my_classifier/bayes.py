9a6cbb01b5d9feca96616dc5fa7d808407216cd3
Lukas Manduch <manduch.lukas@gmail.com>
2017-03-11 20:25:23 +0100

    All classifier methods now works with sqlite

--- naive_bayes/my_classifier/bayes.py
+++ naive_bayes/my_classifier/bayes.py
@@ -1,34 +1,120 @@
 import re
+import sqlite3
+
+"""
+Classifier class holds statistics about frequency of words in documents
+and then uses some Classifier ( naive bayes or fisher alorithm ) to guess
+probability of given text beeing in concrete category
+
+Tables:
+CREATE TABLE IF NOT EXISTS
+feature_counts (
+feature_id INTEGER PRIMARY KEY,
+word text,
+category_id INTEGER NOT NULL,
+count INTEGER NOT NULL DEFAULT(0))
+
+CREATE TABLE IF NOT EXISTS
+category_counts (
+category_id INTEGER PRIMARY KEY,
+name text NOT NULL,
+count INTEGER NOT NULL DEFAULT(0))
+
+"""
 class MyClassifier:
-	def __init__(self):
+	def __init__(self, db_name: str):
 		self.future_count = {}
 		self.classification_used = {}
+		self.db_connection = sqlite3.connect(db_name)
+		self.db_cursor = self.db_connection.cursor()
+		self.db_cursor.execute("CREATE TABLE IF NOT EXISTS"
+				       " feature_counts ("
+				       "feature_id INTEGER PRIMARY KEY,"
+				       "word text,"
+				       "category_id INTEGER NOT NULL,"
+				       "count INTEGER NOT NULL DEFAULT(0))")
+		self.db_cursor.execute("CREATE TABLE IF NOT EXISTS "
+				       "category_counts ("
+		                       "category_id INTEGER PRIMARY KEY,"
+		                       "name text NOT NULL,"
+		                       "count INTEGER NOT NULL DEFAULT(0))")
 
-	def increment_feauture(self, feauture, category):
-		self.future_count.setdefault(feauture, {})
-		self.future_count[feauture].setdefault(category, 0)
-		self.future_count[feauture][category] += 1
+
+	def __del__(self):
+		self.db_connection.commit()
+
+
+	def increment_feature(self, feature, category):
+		cat = self._get_category(category)
+		cat_id = int()
+		if len(cat) == 0:
+			cat_id = self._create_category(category, 0)
+		else:
+			cat_id = cat[0]
+		count = self.get_feature_count(feature, category)
+		# if count is 0 then entry doesn't exist
+		if count == 0:
+			self.db_cursor.execute("INSERT INTO feature_counts "
+			                       "(word, category_id, count) "
+			                       "VALUES "
+			                       "(?, ?, ?) ",
+			                       (feature, cat_id, count + 1))
+		else:
+			self.db_cursor.execute("UPDATE feature_counts "
+			                       "SET count=? WHERE "
+			                       "category_id=? AND word=? ",
+			                       (count + 1, cat_id, feature))
+
 
 	def increment_category(self, category):
-		self.classification_used.setdefault(category, 0)
-		self.classification_used[category] += 1
+		count = self.get_category_count(category)
+		if count == 0:
+			self._create_category(category, 1)
+		else:
+			self.db_cursor.execute("UPDATE category_counts SET "
+			                       "count=? WHERE name=? ",
+			                       (count, category))
 
-	def get_feauture_count(self, feauture, category) -> float:
-		if feauture in self.future_count \
-			and category in self.future_count[feauture]:
-			return float(self.future_count[feauture][category])
-		return float(0)
+
+	def get_feature_count(self, feauture, category) -> float:
+		self.db_cursor.execute(
+			"SELECT category_id FROM category_counts WHERE"
+			" name=?", (category,))
+		id = self.db_cursor.fetchone()
+		if id == None:
+			return 0
+		self.db_cursor.execute("SELECT count from feature_counts "
+		                       "WHERE category_id=? LIMIT 1", (float(
+			id[0]), ))
+		count = self.db_cursor.fetchone()
+		if count == None:
+			return 0
+		return float(count[0])
+
 
 	def get_category_count(self, category) -> float:
-		if category in self.classification_used:
-			return self.classification_used[category]
-		return float(0)
+		self.db_cursor.execute(
+			"SELECT count FROM category_counts WHERE"
+			" name=?", (category,))
+		id = self.db_cursor.fetchone()
+		if id == None:
+			return 0
+		return float(id[0])
+
 
 	def total_count(self):
-		return sum(self.classification_used.values())
+		res = self.db_cursor.execute("SELECT sum(count) FROM "
+		                        "category_counts").fetchone()
+		if res == None:
+			return 0
+		return float(res[0])
+
 
 	def categories(self):
-		return self.classification_used.keys()
+		cats = self.db_cursor.execute("SELECT name from "
+		                              "category_counts")
+		return [cat[0] for cat in cats]
+
 
 	def _get_words(self, content: str):
 		splitter = re.compile('\\W*')
@@ -37,8 +123,26 @@ class MyClassifier:
 		# Return the unique set of words only
 		return dict([(w, 1) for w in words]) # Only unique words
 
+
 	def train(self, content, category):
 		cont = self._get_words(content)
 		for a in cont:
-			self.increment_feauture(a, category)
+			self.increment_feature(a, category)
 		self.increment_category(category)
+
+
+	def _get_category(self, category: str) -> list:
+		self.db_cursor.execute(
+			"SELECT * FROM category_counts WHERE"
+			" name=?", (category,))
+		row = self.db_cursor.fetchone()
+		if row == None:
+			return list()
+		return list(row)
+
+
+	def _create_category(self, category, count) -> int:
+		self.db_cursor.execute("INSERT INTO category_counts (name , "
+		                       "count) VALUES (?, ?)", (category,
+		                                                count ))
+		return self.db_cursor.lastrowid
