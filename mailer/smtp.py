7b0b319bfc0a0650f0fbbe865ad9a800372b97d7
Lukas Manduch <manduch.lukas@gmail.com>
2017-04-09 20:32:39 +0200

    Create smtp sender

---
+++ mailer/smtp.py
@@ -0,0 +1,29 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+
+class Smtp:
+    def __init__(self, host, login, password):
+        self.connection = smtplib.SMTP_SSL(host)
+        self.connection.ehlo()
+        # self.connection.set_debuglevel(1)
+        self.connection.login(login, password)
+        self.message = MIMEMultipart()
+
+    def __del__(self):
+        # self.connection.quit()
+        pass
+
+    
+    def set(self, name, content):
+        self.message[name] = content
+
+    def set_body(self, content):
+        self.message.attach(MIMEText(content, 'plain'))
+
+    def send(self):
+        self.connection.sendmail(self.message['From'],
+                                 self.message['To'],
+                                 self.message.as_string())
