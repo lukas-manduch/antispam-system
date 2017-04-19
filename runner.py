810dd217cde3d3cf811bd6e0ea7f677cc750c1c1
Lukas Manduch <manduch.lukas@gmail.com>
2017-04-19 11:19:25 +0200

    Runner1 - pick chatbot or use hardcoded response

--- runner.py
+++ runner.py
@@ -43,36 +43,55 @@ while p.next():
             if len(message) < 2:
                 print("Too short message, skipping")
                 continue
-            
+            reply_to_message = message # Will be used in email
+            # Detect language
             english_set = set(w.lower()
                                 for w in nltk.corpus.words.words()) 
             text_set = set( w.lower() for w in
                               re.sub( '\W', ' ', message).split()
                               if w[0].isalpha() )
             unusual = text_set.difference(english_set)
-            if len(unusual)*2 > len(text_set):
+            if len(unusual)*3 > len(text_set):
                 print("Translating...")
                 translate = YandexTranslate('trnsl.1.1.20170406T225123Z.cb036d7ebd624db9.8835c34234d938773f407ac77a601e10fc1e1771')
                 message = translate.translate(message, 'en')['text'][0]
+            # Classify
+            if e.get_subject().lower().find("re:") == -1:
+                # Use classifier
                 pass
-            tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-            sentences = tokenizer.tokenize(message)
-            sentence = str()
-            smallest = 10000
-            for s in sentences:
-                l = 50 - len(s)
-                l *= l
-                if smallest > l:
-                    sentence = s
-                    smallest = l
-            sentence = re.sub('\W', ' ', sentence)
-            print("Idenitified sentence")
-            print(sentence)
-            cw = CleverWrap("CC1mmFptGklBB3wFbRtjhqGw7AA")
-            print("Req : " + sentence)
-            reply = (cw.say(sentence))
+            # Pick chatbot
+            if message.lower().find("address") != -1:
+                reply = "Here are data you requested\n" \
+                        "%s\n%s\n%s" % (pers['name'] + " " +
+                                        pers['surname'],
+                                        pers['address'],
+                                        pers['email'])
+            else:
+                print("Using CLEVERBOT")
+                # Pick sentence to reply to
+                tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+                sentences = tokenizer.tokenize(message)
+                sentence = str()
+                smallest = 10000
+                for s in sentences:
+                    l = 50 - len(s)
+                    l *= l
+                    if smallest > l:
+                        sentence = s
+                        smallest = l
+                sentence = re.sub('\W', ' ', sentence)
+                print("Idenitified sentence")
+                print(sentence)
+                # Get CLEVERBOT reply
+                cw = CleverWrap("CC1mmFptGklBB3wFbRtjhqGw7AA")
+                print("Req : " + sentence)
+                reply = (cw.say(sentence))
+
             print("Got reply")
             print(reply)
+            # Personalize
+            reply = reply + "\n\n%s\n" % (pers['name'])
+            # Send response
             s = Smtp(pers['smtp-server'],
                      pers['smtp-login'],
                      pers['smtp-password'])
@@ -83,8 +102,5 @@ while p.next():
                 e.get_sender(),
                 e.get_date(),
                 e.get_subject(),
-                message))
+                reply_to_message))
             s.send()
-
-
-                        
